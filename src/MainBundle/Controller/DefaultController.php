326f07a58c61eb06e6c0f9eb7fa4f2b828529259
DamienChiboub <contact@damienchiboub.com>
2016-03-17 18:05:35 +0100

    Tutoriel 9 - Les Formulaires sans entité sur Symfony 3

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -4,10 +4,41 @@ namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('MainBundle:Default:index.html.twig');
+    }
+
+    public function testAction(Request $request)
+    {
+
+	$name = $email = $objet =  NULL;
+
+	$form = $this->createFormBuilder()
+         ->add('name')
+	 ->add('email')
+	 ->add('objet', ChoiceType::class, array('choices'  => array('' => null,'Yes' => "Yes",'No' => "No", 'Why Not' => "Why Not")))
+         ->add('send', SubmitType::class , array('label' => 'Ok'))
+         ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+        $name = $form["name"]->getData();
+	$email = $form["email"]->getData();
+	$objet = $form["objet"]->getData();
+        }
+
+
+        return $this->render('MainBundle:Default:test.html.twig', array('form'  => $form->createView(),
+									'name'  => $name,
+									'email' => $email,
+									'objet' => $objet));
     }
 }
