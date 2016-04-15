c8ed2cddba4b031cca1d097f301e829a7c1e046e
DamienChiboub <contact@damienchiboub.com>
2016-04-15 03:09:02 +0200

    Tutoriel 16 Ajouter un flashbag à l’action Logout Symfony 3

--- src/MainBundle/Controller/SecurityController.php
+++ src/MainBundle/Controller/SecurityController.php
@@ -5,9 +5,20 @@ namespace MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-
 class SecurityController extends Controller
 {
+
+
+    public function logoutAction()
+    {
+	if ($this->get('security.token_storage')->getToken()->getUser()) {
+	$this->get('security.token_storage')->setToken(null);
+	$this->addFlash('logout','Logout');
+	return $this->redirectToRoute('re_login');
+	}
+    }
+
+
 
     public function loginAction(Request $request)
     {
@@ -23,7 +34,7 @@ class SecurityController extends Controller
 
 
         return $this->render('MainBundle:Security:login.html.twig', array('last_username' => $lastUsername,
-                                                                          'error'         => $error ));
+                                                                          'error'         => $error));
     }
 
 
